a1fdd1bb5a3a586acb5abf193669b59693e462a4
prsathmrc <180431916+prsathmrc@users.noreply.github.com>
2026-01-08 15:41:57 +0000

    [PIL-2723] Deleted step.

--- src/test/scala/uk/gov/hmrc/api/Specs/AccountActivitySpec.scala
+++ src/test/scala/uk/gov/hmrc/api/Specs/AccountActivitySpec.scala
@@ -20,7 +20,6 @@ import uk.gov.hmrc.api.Specs.tags.ApiAcceptanceTests
 import uk.gov.hmrc.api.specdef.AuthSteps.generateBearerToken
 import uk.gov.hmrc.api.specdef.CommonSteps.{assertStatusCode, validateJsonSchemaFor}
 import uk.gov.hmrc.api.specdef.ObligationsAndSubmissionSteps.getObligationsAndStoreResult
-import uk.gov.hmrc.api.specdef.TestOrganisationSteps.{thenIVerifyTheResponseContainsTheFollowingValues, whenIMakeAPICallToCreate, whenIMakeAPICallToDeleteOrganisationUsing}
 
 class AccountActivitySpec extends BaseSpec {
 
@@ -47,10 +46,6 @@ class AccountActivitySpec extends BaseSpec {
 
       Then("I validate Obligation Response json schema for Obligations_And_Submission_200")
       validateJsonSchemaFor("AccountActivity", "Response", "Account_Activity_200")
-
-      When("I make API call to delete organisation using setup/organisation with XMPLR0000000012")
-      whenIMakeAPICallToDeleteOrganisationUsing("setup/organisation", "XMPLR0000000012")
-
     }
 
     Scenario("Verify Account Activity response for 400 and validate schema", ApiAcceptanceTests) {
@@ -71,10 +66,6 @@ class AccountActivitySpec extends BaseSpec {
 
       Then("I validate Obligation Response json schema for Obligations_And_Submission_200")
       validateJsonSchemaFor("AccountActivity", "Response", "Account_Activity_Error_Response")
-
-      When("I make API call to delete organisation using setup/organisation with XMPLR0000000012")
-      whenIMakeAPICallToDeleteOrganisationUsing("setup/organisation", "XMPLR0000000012")
-
     }
 
     Scenario("Verify Account Activity response for 422 and validate schema", ApiAcceptanceTests) {
@@ -95,10 +86,6 @@ class AccountActivitySpec extends BaseSpec {
 
       Then("I validate Obligation Response json schema for Obligations_And_Submission_200")
       validateJsonSchemaFor("AccountActivity", "Response", "Account_Activity_Error_Response")
-
-      When("I make API call to delete organisation using setup/organisation with XEPLR0000422003")
-      whenIMakeAPICallToDeleteOrganisationUsing("setup/organisation", "XEPLR0000422003")
-
     }
 
     Scenario("Verify Account Activity response for 500 and validate schema", ApiAcceptanceTests) {
@@ -119,11 +106,6 @@ class AccountActivitySpec extends BaseSpec {
 
       Then("I validate Obligation Response json schema for Obligations_And_Submission_200")
       validateJsonSchemaFor("AccountActivity", "Response", "Account_Activity_Error_Response")
-
-      When("I make API call to delete organisation using setup/organisation with XEPLR0123456500")
-      whenIMakeAPICallToDeleteOrganisationUsing("setup/organisation", "XEPLR0123456500")
-
     }
-
   }
 }
