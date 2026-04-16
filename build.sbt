25b89d415f8743e29c2112db9cd53b9dbfb4e8c0
Kevin Coatsworth <29205960+kevincoatsworth@users.noreply.github.com>
2026-04-16 14:01:21 +0100

    PIL-2943: Upgraded library and dependencies to be up to date. I've only bumped scala by minor version (Scala 2) as there's a separate ticket for upgrading to Scala 3 (PIL-2941).

--- build.sbt
+++ build.sbt
@@ -11,7 +11,7 @@ lazy val testSuite = (project in file("."))
   .settings(
     name := "pillar2-api-tests",
     version := "0.1.0",
-    scalaVersion := "2.13.12",
+    scalaVersion := "2.13.16",
     scalacOptions ++= Seq("-feature"),
     libraryDependencies ++= Dependencies.test,
     resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2"),
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,20 +3,20 @@ import sbt.*
 object Dependencies {
 
   val guiceVersion = "7.20.1"
-  val circeVersion = "0.14.10"
+  val circeVersion = "0.14.15"
 
   val test: Seq[ModuleID] = (Seq(
-    "uk.gov.hmrc"       %% "api-test-runner"         % "0.8.0",
+    "uk.gov.hmrc"       %% "api-test-runner"         % "0.10.0",
     "com.google.inject"  % "guice"                   % "7.0.0",
     "junit"              % "junit"                   % "4.13.2",
     "com.github.sbt"     % "junit-interface"         % "0.13.3",
-    "org.scalatest"     %% "scalatest"               % "3.2.19",
-    "com.typesafe.play" %% "play-json"               % "2.10.6",
+    "org.scalatest"     %% "scalatest"               % "3.2.20",
+    "com.typesafe.play" %% "play-json"               % "2.10.8",
     "io.circe"          %% "circe-json-schema"       % "0.2.0",
     "io.circe"          %% "circe-core"              % circeVersion,
     "io.circe"          %% "circe-generic"           % circeVersion,
     "io.circe"          %% "circe-parser"            % circeVersion,
-    "uk.gov.hmrc"       %% "http-verbs-test-play-30" % "15.1.0"
+    "uk.gov.hmrc"       %% "http-verbs-test-play-30" % "15.8.0"
   ) ++ Seq("slf4j", "serialization-jackson", "actor-typed", "protobuf-v3", "stream")
     .map(lib => "org.apache.pekko" %% s"pekko-$lib" % "1.1.2"))
     .map(_ % Test)
