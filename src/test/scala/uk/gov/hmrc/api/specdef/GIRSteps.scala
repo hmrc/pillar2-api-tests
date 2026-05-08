6274fa57900b84e83f39e8beaa0808f75bb2daa0
prsathmrc <180431916+prsathmrc@users.noreply.github.com>
2026-05-08 13:26:49 +0100

    [PIL-2974] Formatted GIR steps

--- src/test/scala/uk/gov/hmrc/api/specdef/GIRSteps.scala
+++ src/test/scala/uk/gov/hmrc/api/specdef/GIRSteps.scala
@@ -4,8 +4,6 @@ import uk.gov.hmrc.api.pages.{StateStoragePage, UKTRPage}
 
 object GIRSteps {
 
-
   def givenIMakeAPICallTo(requestApi: String, endpoint: String, pillarID: String, statusCode: String): Unit =
     StateStoragePage.setResponseCode(UKTRPage.sendUKTRRequest(requestApi, endpoint, pillarID, statusCode))
-
 }
